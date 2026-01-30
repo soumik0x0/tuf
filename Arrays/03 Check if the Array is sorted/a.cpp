26824140f762662996ee3e7606bda4427ead7635
soumik0x0 <iamrealsoumik@gmail.com>
2026-01-30 10:32:59 +0530

    30 Jan 2026

---
+++ Arrays/03 Check if the Array is sorted/a.cpp
@@ -0,0 +1,44 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    Solution sol;
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+
+        vector<int> nums(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> nums[i];
+        }
+
+        cout << boolalpha << sol.check(nums) << "\n";
+    }
+
+    return 0;
+}
